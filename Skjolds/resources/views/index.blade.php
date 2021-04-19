7600ed1131eb4be4763b23f059b5bba2be6c0fdc
MelbouVP <saurfanng@gmail.com>
2021-04-19 19:36:39 +0300

    Added gender category section to index page

--- Skjolds/resources/views/index.blade.php
+++ Skjolds/resources/views/index.blade.php
@@ -52,37 +52,109 @@
         <main>
             <section class="welcome-section">
 
-                <div class="welcome-section__content">
-                    <div class="content__text">
-                        <div class="content__text--sup-title">
-                            <p>
-                                Enjoy this season with
-                            </p>
+                <div class="welcome-section__container">
+                    <div class="welcome-section__content">
+                        <div class="content__text">
+                            <div class="content__text--sup-title">
+                                <p>
+                                    Enjoy this season with
+                                </p>
+                            </div>
+                            <div class="content__text--title">
+                                Skjolds clothing
+                            </div>
+                            <div class="content__text--sub-title">
+                                Release your inner passion through fashion.
+                            </div>
                         </div>
-                        <div class="content__text--title">
-                            Skjolds clothing
-                        </div>
-                        <div class="content__text--sub-title">
-                            Release your inner passion through fashion.
+    
+                        <div class="content__action-btn">
+                            <a href="/shop" class="content__action-btn--link">
+                                Shop now
+                            </a>
                         </div>
                     </div>
-
-                    <div class="content__action-btn">
-                        <a href="/shop" class="content__action-btn--link">
-                            Shop now
-                        </a>
+    
+                    <div class="welcome-section__banner-img">
+                        <img src="/images/hero-img.png" alt="">
+                    </div>
+                    <div class="welcome-section__half-elipse">
+                        <img src="/images/half-elipse.png" alt="">
+                    </div>
+                    <div class="welcome-section__half-elipse">
+                        <img src="/images/half-elipse.png" alt="">
                     </div>
                 </div>
+                
+            </section>
 
-                <div class="welcome-section__hero-img">
-                    <img src="/images/hero-img.png" alt="">
+            <section class="category-section">
+                <div class="category__container-wrapper">
+
+                    <div class="category__container">
+                        
+                        <div class="category__container--title">
+                            <h1>Explore latest trends</h1>
+                        </div>
+
+                        <div class="category__container--content">
+                            <div class="gender-card__container">
+                                <div class="gender-card__description">
+                                    <div class="description__sup-title">
+                                        Discover
+                                    </div>
+                                    <div class="description__title">
+                                        Men
+                                    </div>
+                                    <div class="description__text">
+                                        <p>Fashion for the sophisticated men.</p>
+                                        <p>Skjolds offers newest collections that follow newest trends within male fashion.</p>
+                                    </div>
+                                    <div class="description__action-btn">
+                                        <a href="" class="description__action-btn--link" >
+                                            Browse
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="gender-card__gender-img">
+                                    <img src="/images/male-category-img.png" alt="Image of a man">
+                                    
+                                </div>
+                            </div>
+
+                            <div class="gender-card__container">
+                                <div class="gender-card__description">
+                                    <div class="description__sup-title">
+                                        Discover
+                                    </div>
+                                    <div class="description__title">
+                                        Women
+                                    </div>
+                                    <div class="description__text">
+                                        <p>Skjolds showcases designer collections from variety of seasons and styles.</p>
+                                        <p>Find the inner passion in women's collections.</p>
+                                    </div>
+                                    <div class="description__action-btn">
+                                        <a href="" class="description__action-btn--link" >
+                                            Browse
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="gender-card__gender-img">
+                                    <img src="/images/female-category-img.png" alt="Image of a woman">
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    
                 </div>
-                <div class="welcome-section__half-elipse">
-                    <img src="/images/half-elipse.png" alt="">
-                </div>
-                <div class="welcome-section__half-elipse">
-                    <img src="/images/half-elipse.png" alt="">
-                </div>
+            </section>
+
+
+            <section class="feature-section">
+                
             </section>
         </main>
     </body>
